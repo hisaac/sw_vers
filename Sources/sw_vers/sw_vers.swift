32b6d996ef57585a1c0f0b2a21766d23837e8a86
Isaac Halvorson <hello@hisaac.net>
2024-02-24 20:42:41 -0600

    Add comment

--- Sources/sw_vers/sw_vers.swift
+++ Sources/sw_vers/sw_vers.swift
@@ -1,5 +1,8 @@
 import Foundation
 
+/// Just a fun experiment to reimplement macOS' `sw_vers` binary in Swift
+/// The original is written in C, and is here: https://opensource.apple.com/source/DarwinTools/DarwinTools-1/sw_vers.c.auto.html
+
 @main
 final class sw_vers {
 	static func main() throws {
